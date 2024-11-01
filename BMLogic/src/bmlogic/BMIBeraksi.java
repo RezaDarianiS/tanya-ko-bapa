629470bfccbe7299908dcf02da4e45f4bebdcec9
RezaDarianiS <echadariani0901@gmail.com>
2024-11-01 06:31:32 -0700

    Add files via upload

---
+++ BMLogic/src/bmlogic/BMIBeraksi.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bmlogic;
+
+/**
+ *
+ * @author les
+ */
+public class BMIBeraksi {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        BMLogic nih = new BMLogic(38,1.55);
+        nih.berats();
+        nih.hasil();
+    }
+    
+    
+}
